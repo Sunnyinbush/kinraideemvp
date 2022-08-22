3ed0d47eb154c73f1867c5948a9c528afa5e9bef
Vorapon Kachapornkul <hakao97@gmail.com>
2022-08-22 09:52:40 +0700

    Remove chinese comments

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ line_bot_api = LineBotApi('reGGOrpOZ5RafgV+VqQHiULKInLdOq4gTKa3npfHWNzorpMn/V85a
 # Channel Secret
 handler = WebhookHandler('9c727591b56bf0a08a4a404605a97f2c')
 
-# 監聽所有來自 /callback 的 Post Request
+# callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -30,7 +30,6 @@ def callback():
         abort(400)
     return 'OK'
 
-# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
