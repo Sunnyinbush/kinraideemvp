922bf588465e21902f44cd25df6547ec92104f04
Vorapon Kachapornkul <hakao97@gmail.com>
2022-08-26 09:04:58 +0700

    Working on random backend process

--- routeprocess.py
+++ routeprocess.py
@@ -7,9 +7,16 @@ from jsonstorage import *
 
 def backendprocess(inputword):
     triggerwords = ['กินอะไรดี','กินไรดี', 'kinraidee', 'KinRaiDee', 'Kinraidee']
-#have some random function to select restaurant
-
+    example_message = ['คำตัวอย่าง','example']
+    #have some random function to select restaurant
     if inputword in triggerwords:
+        payload = randomselectrestaurant()
+        flex_message = FlexSendMessage(
+            type = 'flex',
+            alt_text=payload["body"]["content"]["text"],
+            contents=json.loads(payload)
+    )
+    elif inputword in example_message:
         flex_message = FlexSendMessage(
             type = 'flex',
             alt_text='CU iCanteen',
@@ -17,8 +24,12 @@ def backendprocess(inputword):
     )
     return flex_message
 
-def randomselectrestaurant(place, list):
-    pass
+def randomselectrestaurant():
+    #open csv file as list
+    with open('restaurant.csv', 'r') as csvfile:
+        restaurantlist = csvfile.readlines()
+    #random select restaurant
+    randomrestaurant = random.choice(restaurantlist)
 
 def convertjsontostring(json):
     return json.dumps(json)
