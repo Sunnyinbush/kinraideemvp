65911330029b455a7ad1b778495a6cba49ec3555
Vorapon Kachapornkul <hakao97@gmail.com>
2022-08-31 10:47:19 +0700

    Clean up first MVP code

--- routeprocess.py
+++ routeprocess.py
@@ -6,6 +6,33 @@ from linebot.models import *
 
 from jsonstorage import *
 
+def backendprocess(inputword):
+    randomtrigger = ['กินอะไรดี','กินไรดี', 'kinraidee', 'KinRaiDee', 'Kinraidee']
+    example_message = ['คำตัวอย่าง','example']
+    if inputword in randomtrigger:
+        flex_message = randomprocess()
+    elif inputword in example_message:
+        print(json.loads(flex_message_json_example))
+        flex_message = FlexSendMessage(
+            type = 'flex',
+            alt_text='CU iCanteen',
+            contents=json.loads(flex_message_json_example)
+    )
+    else:
+        flex_message = None
+    return flex_message
+
+def randomprocess():
+    payload = randomselectrestaurant()
+    json_payload = json.dumps(payload)
+    print(json.loads(json_payload))
+    flex_message = FlexSendMessage(
+        type = 'flex',
+        alt_text=payload["body"]["contents"][0]["text"],
+        contents=json.loads(json_payload)
+    )
+    return flex_message
+
 def randomselectrestaurant():
     #open csv file as list
     randomselectrestaurant.var = open("restaurantlist.csv", "r")
@@ -33,33 +60,3 @@ def convertjsontostring(restaurant):
     #open time
     final_json['body']['contents'][1]['contents'][1]['contents'][1]['text'] = restaurant[4].strip()
     return final_json
-
-def randomprocess(inputword):
-    pass
-
-def backendprocess(inputword):
-    triggerwords = ['กินอะไรดี','กินไรดี', 'kinraidee', 'KinRaiDee', 'Kinraidee']
-    example_message = ['คำตัวอย่าง','example']
-    #have some random function to select restaurant
-    if inputword in triggerwords:
-        payload = randomselectrestaurant()
-        json_payload = json.dumps(payload)
-        print(json.loads(json_payload))
-        flex_message = FlexSendMessage(
-            type = 'flex',
-            alt_text=payload["body"]["contents"][0]["text"],
-            contents=json.loads(json_payload)
-    )
-        
-    elif inputword in example_message:
-        print(json.loads(flex_message_json_example))
-        flex_message = FlexSendMessage(
-            type = 'flex',
-            alt_text='CU iCanteen',
-            contents=json.loads(flex_message_json_example)
-    )
-    else:
-        flex_message = None
-    return flex_message
-
-backendprocess('kinraidee')
