c97b90ace8082853106feddb087d4d70631715c8
Vorapon Kachapornkul <hakao97@gmail.com>
2022-09-13 13:56:24 +0700

    Optimized Route Process v2

--- routeprocess.py
+++ routeprocess.py
@@ -6,51 +6,44 @@ from linebot.models import *
 
 from jsonstorage import *
 
-def backendprocess(inputword):
+def backendrouter(textinput):
     triggerdictionary = {
         "สามย่าน" : "restaurantlist_samyan.csv",
         "MBK" : 'restaurantlist_mbk.csv',
         "สยาม" : 'restaurantlist_siam.csv',
     }
     example_message = ['คำตัวอย่าง','example']
-    inputword = inputword.split(" ")
-    if inputword[1] in triggerdictionary:
-        flex_message = randomprocess(triggerdictionary[inputword[1]])
-    elif inputword in example_message:
-        print(json.loads(flex_message_json_example))
-        flex_message = FlexSendMessage(
-            type = 'flex',
-            alt_text='CU iCanteen',
-            contents=json.loads(flex_message_json_example)
+    input_word_list = textinput.split(" ")
+    if input_word_list[0] == "กินอะไรดีที่":
+        if input_word_list[1] in triggerdictionary:
+            flex_message = randomprocess(triggerdictionary[input_word_list[1]])
+        elif input_word_list[1] in example_message:
+            print(json.loads(flex_message_json_example))
+            flex_message = FlexSendMessage(
+                type = 'flex',
+                alt_text='CU iCanteen',
+                contents=json.loads(flex_message_json_example)
     )
-    else:
-        flex_message = None
+        else:
+            flex_message = None
     return flex_message
 
 def randomprocess(location):
-    payload = randomselectrestaurant(location)
-    json_payload = json.dumps(payload)
-    print(json.loads(json_payload))
-    flex_message = FlexSendMessage(
-        type = 'flex',
-        alt_text=payload["body"]["contents"][0]["text"],
-        contents=json.loads(json_payload)
-    )
-    return flex_message
-
-def randomselectrestaurant(location):
-    #open csv file as list
-    randomselectrestaurant.var = open(location, "r")
-    # with open('restaurantlist.csv', 'r') as datafile:
-    data = csv.reader(randomselectrestaurant.var, delimiter=';')
+    randomprocess.var = open(location, "r")
+    data = csv.reader(randomprocess.var, delimiter=';')
     header = next(data)
     table = [row for row in data]
-    #random select restaurant
     choice = random.choice(table)
     randomrestaurant = choice[0].split(',')
     post_data = convertjsontostring(randomrestaurant)
-    randomselectrestaurant.var.close()
-    return post_data
+    randomprocess.var.close()
+    json_payload = json.dumps(post_data)
+    flex_message = FlexSendMessage(
+        type = 'flex',
+        alt_text=post_data["body"]["contents"][0]["text"],
+        contents=json.loads(json_payload)
+    )
+    return flex_message
 
 def convertjsontostring(restaurant):
     final_json = flex_message_json_template
@@ -69,5 +62,3 @@ def convertjsontostring(restaurant):
     if len(restaurant) > 5:
         final_json['footer']['contents'][1]['action']['uri'] = restaurant[5].strip()
     return final_json
-
-backendprocess("กินอะไรดีที่ MBK")
