a938ec08b24c1ec54157c38ee7a4a25cd1f67dfc
Vorapon Kachapornkul <hakao97@gmail.com>
2022-11-11 22:48:16 +0700

    Create new handler functions

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+from oldrouteprocess import *
+
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('reGGOrpOZ5RafgV+VqQHiULKInLdOq4gTKa3npfHWNzorpMn/V85a7axufVRJzmHvhxlGAuJ9wAqI6NMGIHjdL1yMqoFkR3JwoAHrTbNnqiI91pFvfp/AWgnOQCMk2vWEBvbGLgT4POpr+nZOye9ewdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('9c727591b56bf0a08a4a404605a97f2c')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if flex_message != None:
+        print("Flex Message Sent!")
+        line_bot_api.reply_message(event.reply_token, reply_message)
---
+++ oldrouteprocess.py
@@ -0,0 +1,62 @@
+import random
+import json
+import csv
+
+from linebot.models import *
+
+from jsonstorage import *
+
+def backendrouter(textinput):
+    triggerdictionary = {
+        "สามย่านมิตรทาวน์" : "restaurantlist_samyanmidtown.csv",
+        "MBK" : 'restaurantlist_mbk.csv',
+        "สยามพารากอน" : 'restaurantlist_siamparagon.csv',
+    }
+    example_message = ['คำตัวอย่าง','example']
+    input_word_list = textinput.split(" ")
+    if input_word_list[0] == "กินอะไรดีที่":
+        if input_word_list[1] in triggerdictionary:
+            flex_message = randomprocess(triggerdictionary[input_word_list[1]])
+        elif input_word_list[1] in example_message:
+            flex_message = FlexSendMessage(
+                type = 'flex',
+                alt_text='CU iCanteen',
+                contents=json.loads(flex_message_json_example))
+    else:
+        flex_message = None
+    return flex_message
+
+def randomprocess(location):
+    randomprocess.var = open(location, "r")
+    data = csv.reader(randomprocess.var, delimiter=';')
+    header = next(data)
+    table = [row for row in data]
+    choice = random.choice(table)
+    randomrestaurant = choice[0].split(',')
+    post_data = convertjsontostring(randomrestaurant)
+    randomprocess.var.close()
+    json_payload = json.dumps(post_data)
+    flex_message = FlexSendMessage(
+        type = 'flex',
+        alt_text=post_data["body"]["contents"][0]["text"],
+        contents=json.loads(json_payload)
+    )
+    return flex_message
+
+def convertjsontostring(restaurant):
+    final_json = flex_message_json_template
+    #photo url
+    final_json["hero"]["url"] = restaurant[0].strip()
+    #place title
+    final_json["body"]["contents"][0]["text"] = restaurant[1].strip()
+    #location
+    final_json['body']['contents'][1]['contents'][0]['contents'][1]['text'] = restaurant[2].strip()
+    #open time
+    final_json['body']['contents'][1]['contents'][1]['contents'][1]['text'] = restaurant[3].strip()
+    #navigate button
+    if len(restaurant) > 4:
+        final_json['footer']['contents'][0]['action']['uri'] = restaurant[4].strip()
+    #menu button
+    if len(restaurant) > 5:
+        final_json['footer']['contents'][1]['action']['uri'] = restaurant[5].strip()
+    return final_json
